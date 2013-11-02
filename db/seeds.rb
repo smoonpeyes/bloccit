32890f9a4eef0c7302aeb9d782c5c1a6a5760b7c
Layla <smoonpeyes@gmail.com>
2013-11-02 14:42:39 -0600

    Associated and nested topics and posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
-require 'faker'
+require 'faker'  
+
+# Create 15 topics
+topics = []
+15.times do
+  topics << Topic.create(
+    name: Faker::Lorem.words(rand(1..10)).join(" "), 
+    description: Faker::Lorem.paragraph(rand(1..4))
+  )
+end
 
 rand(4..10).times do
   password = Faker::Lorem.characters(10)
@@ -14,7 +23,18 @@ rand(4..10).times do
   # we create an instance of a user which isn't saved to the database.
   # The `skip_confirmation!` method sets the confirmation date
   # to avoid sending an email. The `save` method updates the database.
+  rand(5..12).times do
+    topic = topics.first # getting the first topic here
+    p = u.posts.create(
+      topic: topic,
+      title: Faker::Lorem.words(rand(1..10)).join(" "), 
+      body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
+    # set the created_at to a time within the past year
+    p.update_attribute(:created_at, Time.now - rand(600..31536000))
 
+    topics.rotate!
+  end
+  
   rand(5..12).times do
     p = u.posts.create(
       title: Faker::Lorem.words(rand(1..10)).join(" "), 
@@ -29,9 +49,31 @@ rand(4..10).times do
   end
 end
 
-u = User.first
-u.skip_reconfirmation!
-u.update_attributes(email: 'smoonpeyes@gmail.com', password: 'helloworld', password_confirmation: 'helloworld')
+u = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'admin')
+
+u = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'moderator')
+
+u = User.new(
+  name: 'Member User',
+  email: 'member@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
 
 puts "Seed finished"
 puts "#{User.count} users created"
