2c23d50748a1ea13c7fc583d378ca3bc3ca8a2de
Layla <smoonpeyes@gmail.com>
2013-11-02 12:52:59 -0600

    Implemented Cancan and basic roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,17 @@ class User < ActiveRecord::Base
 
   has_many :posts
 
+  before_create :set_member
+
+  ROLES = %w[member moderator admin]
+    def role?(base_role)
+      role.nil? ? false : ROLES.index(base_role.to_s) <= ROLES.index(role)
+  end  
+
+  private
+
+  def set_member
+    self.role = 'member'
+  end
+
 end
