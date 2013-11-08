3fc463116435deac4adb5f3c45f34a9527600bf1
vagrant <vagrant@rails-dev-box>
2013-11-08 15:18:57 +0000

    Added comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Bloccit::Application.routes.draw do
 
-  devise_for :users
-
-  resources :topics do
-    resources :posts, except: [:index]
+resources :topics do
+    resources :posts, except: [:index] do
+      resources :comments, only: [:create, :destroy]
+    end
   end
+
+  devise_for :users
 
   match "about" => 'welcome#about', via: :get
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-require 'faker'  
+require 'faker'
 
 # Create 15 topics
 topics = []
@@ -8,6 +8,7 @@ topics = []
     description: Faker::Lorem.paragraph(rand(1..4))
   )
 end
+ 
 
 rand(4..10).times do
   password = Faker::Lorem.characters(10)
@@ -19,12 +20,8 @@ rand(4..10).times do
   u.skip_confirmation!
   u.save
 
-  # Note: by calling `User.new` instead of `create`,
-  # we create an instance of a user which isn't saved to the database.
-  # The `skip_confirmation!` method sets the confirmation date
-  # to avoid sending an email. The `save` method updates the database.
   rand(5..12).times do
-    topic = topics.first # getting the first topic here
+    topic = topics.first
     p = u.posts.create(
       topic: topic,
       title: Faker::Lorem.words(rand(1..10)).join(" "), 
@@ -32,21 +29,20 @@ rand(4..10).times do
     # set the created_at to a time within the past year
     p.update_attribute(:created_at, Time.now - rand(600..31536000))
 
+
     topics.rotate!
   end
-  
-  rand(5..12).times do
-    p = u.posts.create(
-      title: Faker::Lorem.words(rand(1..10)).join(" "), 
-      body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
-    # set the created_at to a time within the past year
-    p.update_attribute(:created_at, Time.now - rand(600..31536000))
+end
 
-    rand(3..7).times do
-      p.comments.create(
-        body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
+post_count = Post.count
+User.all.each do |user|
+    rand(30..50).times do
+      p = Post.find(rand(1..post_count))
+      c = user.comments.create(
+        body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"),
+        post: p)
+      c.update_attribute(:created_at, Time.now - rand(600..31536000))
     end
-  end
 end
 
 u = User.new(
