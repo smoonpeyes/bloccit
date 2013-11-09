b1830d646593e35c1f02d8ed9386a7aa8b5a5c96
Layla <smoonpeyes@gmail.com>
2013-11-09 08:37:24 -0700

    Implemented Facebook Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ resources :topics do
     end
   end
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   match "about" => 'welcome#about', via: :get
 
