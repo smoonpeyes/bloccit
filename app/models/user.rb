d6b5d4f6c340bc3d25313acad0462f6d3cd88df9
Layla <smoonpeyes@gmail.com>
2013-11-09 19:49:10 -0700

    Implemented favoriting

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,12 @@ devise :database_authenticatable, :registerable,
          :omniauthable, :omniauth_providers => [:facebook]
 
    attr_accessible :email, :password, :password_confirmation,
-                  :remember_me, :name, :avatar, :provider, :uid
+                  :remember_me, :name, :avatar, :provider, :uid, :email_favorites
 
   has_many :posts
   has_many :comments
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   before_create :set_member
 
@@ -38,6 +39,10 @@ devise :database_authenticatable, :registerable,
       role.nil? ? false : ROLES.index(base_role.to_s) <= ROLES.index(role)
   end  
 
+  def favorited(post)
+    self.favorites.where(post_id: post.id).first
+  end
+  
   private
 
   def set_member
